bbe9eab220fa480480123885164601dc7c17e0c0
chanidapa vantaneeyakul <ambb_bb@hotmail.com>
2017-07-24 00:14:23 +0700

    edit file sort

--- src/Sort.js
+++ src/Sort.js
@@ -3,7 +3,6 @@ import {Nav, NavItem} from 'react-bootstrap'
 class Sort extends Component {
   changeMode(event){
     const {mode} = this.props
-    const list = JSON.parse(localStorage.getItem('data')) || []
 
     if(event.target.id === "all"){
       mode("all")
@@ -15,46 +14,6 @@ class Sort extends Component {
           mode("uncomplete")
         }
     }
-  constructor(props) {
-    super(props)
-
-    var complete   = 0
-		var uncomplete = 0
-    const list = JSON.parse(localStorage.getItem('data')) || []
-
-		var length = list.filter((item)=>{
-			if(item.status === true) return (complete += 1)
-			if(item.status === false) return (uncomplete += 1)
-		})
-    this.state = {
-      all: list.length,
-      complete: complete,
-      uncomplete: uncomplete
-    }
-
-  }
-  changeMode(event){
-    const {mode} = this.props
-    const todos = JSON.parse(localStorage.getItem('data')) || []
-    this.setState = {
-        all: todo.length,
-        complete: com,
-        uncomplete: un
-    }
-    if(event.target.id === "all"){
-        mode("all")
-        countTodo( this.state )
-    }else{
-        if(event.target.id === "complete"){
-          mode("complete")
-          countTodo( this.state )
-        }
-        else if(event.target.id === "uncomplete"){
-          mode("uncomplete")
-          countTodo( this.state )
-        }
-    }
-    
   }
 
   render() {
@@ -65,10 +24,6 @@ class Sort extends Component {
         <NavItem onClick={this.changeMode.bind(this)} id='complete'>Complete</NavItem>
         <NavItem onClick={this.changeMode.bind(this)} id='uncomplete'>UnComplete</NavItem>
         </Nav>
-          <NavItem onClick={this.changeMode.bind(this)} id='all'>All {this.state.all}</NavItem>
-          <NavItem onClick={this.changeMode.bind(this)} id='complete'>Complete {this.state.complete}</NavItem>
-          <NavItem onClick={this.changeMode.bind(this)} id='uncomplete'>UnComplete {this.state.uncomplete}</NavItem>
-        </Nav> 
       </div>
     )
   }
